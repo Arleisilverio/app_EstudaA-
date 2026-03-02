806b4efc85dd70ac8d2c0b1559be9a02e8f6b1d4
[dyad] <arleisilverio41@gmail.com>
2026-03-02 18:42:40 -0300

    [dyad] Configurando suporte total ao modo escuro em todo o aplicativo - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,9 +8,9 @@ import BottomNav from "@/components/BottomNav";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-[#FAF6F1] flex flex-col max-w-md mx-auto relative overflow-x-hidden">
+    <div className="min-h-screen bg-background flex flex-col max-w-md mx-auto relative overflow-x-hidden">
       {/* Background Decorative Elements */}
-      <div className="absolute top-0 right-0 w-64 h-64 bg-study-light/30 rounded-full -translate-y-1/2 translate-x-1/2 blur-3xl" />
+      <div className="absolute top-0 right-0 w-64 h-64 bg-study-primary/5 dark:bg-study-primary/10 rounded-full -translate-y-1/2 translate-x-1/2 blur-3xl" />
       <div className="absolute bottom-40 left-0 w-48 h-48 bg-study-primary/5 rounded-full -translate-x-1/2 blur-2xl" />
       
       <div className="relative z-10 flex flex-col h-full">
